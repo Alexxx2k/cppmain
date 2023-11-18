0d6231d3d81789ff3b042fdd44bf651159051239
AlexNik <sahan@DESKTOP-R8OTJOJ>
2023-11-18 23:57:21 +0300

    done

---
+++ Labs/Lab6.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+
+void outArray(int* arrMatrix, int& rows, int& cols);
+void printArray(int* arrMatrix, int& rows, int& cols);
+
+int main()
+{
+  int rows = 0;
+  int cols = 0;
+
+  if (!(std::cin >> rows >> cols))
+  {
+    std::cerr << ("Wrong Input");
+  }
+  int* arr = new int[rows * cols];
+
+  outArray(arr, rows, cols);
+  printArray(arr, rows, cols);
+
+  delete[] arr;
+}
+
+void outArray(int *arrMatrix, int& rows, int& cols)
+{
+  for (int i = 0; i < rows * cols; i++) {
+    if (!(std::cin >> arrMatrix[i]))
+    {
+      std::cerr << ("Wrong Input");
+      exit(0);
+    }
+  }
+}
+void printArray(int* arrMatrix, int& rows, int& cols){
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      std::cout << std::setw(4) << arrMatrix[i * rows + j];
+    }
+    std::cout << "\n";
+  }
+}
